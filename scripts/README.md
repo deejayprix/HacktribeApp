fdf7292e4e197eb2dca63ab71e2b2591a44df4a6
deejayprix <deejayprix@gmail.com>
2025-12-11 23:36:00 +0100

    Scripte+Update

---
+++ scripts/README.md
@@ -0,0 +1,23 @@
+# e2-scripts
+
+Scripts for dealing with electribe2 files.
+
+e2all2pat.py  - Converts allpat file to a directory of e2pat files.  
+
+e2pat2syx.py - Converts a pattern file in e2pat format to a pattern file in sysex format.
+
+e2syx2pat.py - Converts a pattern file in sysex format to pattern file in e2pat format.
+
+e2seqrot.py - Rotates sequence of specified part by specified number of steps.  Takes e2pat as input.
+
+e2pat2all.py - Create allpatterns from a single pattern.
+
+e2ev.py - Split event recording by pattern, create stems, mute parts, extract and replace patterns.
+
+e2sysex.py - <-- Moved to [hacktribe-editor](https://github.com/bangcorrupt/hacktribe-editor)
+
+e2-header.py - Edit file header to load sampler firmware on synth hardware
+
+e2-init-pat.py - Insert custom init pattern in electribe 2 sampler firmware version 2.02
+
+e2_syx_codec.py - <-- Moved to [hacktribe-editor](https://github.com/bangcorrupt/hacktribe-editor)
---
+++ scripts/hacktribe-tutorial.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+# Hacktribe firmware patch tutorial
+#
+# See https://github.com/bangcorrupt/hacktribe for details.
+
+# This tutorial will help you learn how to run a few bash commands and a python script.
+# For best results, read everything, 
+# run each command separately and pay attention to the output.
+#
+# Anything starting with a '#' character is a comment and ignored by the shell 
+# (you don't need to type these).  
+# We can use comments to disable a command (see line 66 below).
+#
+# You will need to install git, python and pip.
+#
+# Happy hacking!
+
+
+# First we need to download some files.  Clone the repo, including all submodules:
+git clone --recursive https://github.com/bangcorrupt/hacktribe.git
+
+# Then change directory to the 'hacktribe' directory we just downloaded:
+cd hacktribe
+
+# Next we need to download the factory firmware.  
+# This is proprietary and copyrighted.  
+# Please only download it for yourself and do not distribute it.  
+# 
+# 'wget' command can be used to download files if we know the URL:
+wget https://cdn.korg.com/us/support/download/files/0b87bcd3112fbb8c0ad7b0f55e618837.zip
+
+# Unzip the zip archive we just downloaded to access the files inside:
+unzip 0b87bcd3112fbb8c0ad7b0f55e618837.zip
+
+# Move the firmware update file 'SYSTEM.VSB' to our current directory '.' :
+mv electribe_sampler_system_v202/SYSTEM.VSB .
+
+# Now we have all the files in the right places.
+
+# Next, make sure the required python packages are installed:
+pip install argparse bsdiff4
+
+# Now we are ready to run the firmware patching script.
+# This will apply the hacktribe patch to the factory firmware,
+# then check the sha256 hash of the patched file.
+# If the hashes match, you have the same file I tested.
+# If not, you will get a scary error message saying 'patch failed' or something.
+#
+# If you're installing hacktribe to a factory synth (grey or blue) 
+# for the first time, we need to modify the update file header as well.
+# (If you already have a previous version of hacktribe installed, this is not necessary).
+#
+# EITHER: Run the firmware patching script for synth, using '-e' flag to modify header:
+python scripts/e2-firmware-patch.py -e
+
+# OR: Run the firmware patching script for sampler/hacktribe, no flags necessary:
+# python scripts/e2-firmware-patch.py
+
+# If the patching was successful, 
+# you should see 'Firmware patched successfully' in the output from the script.  
+
+# Copy 'hacked-SYSTEM.VSB' to the 'System' directory on the SD card, 
+# rename it to 'SYSTEM.VSB' and run the firmware update function on the device.
+
+# Everything is done nowm the rest is just tidying up.
+
+# Move hacked-SYSTEM.VSB to parent directory:
+
+# mv hacked-SYSTEM.VSB ..
+
+# Change directory to parent directory:
+
+# cd ..
+
+# Delete all the files we downloaded:
+
+# rm -rf hacktribe
+
+# See https://github.com/bangcorrupt/hacktribe/discussions/41 if you are having difficulties
